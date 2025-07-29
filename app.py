5e5ac58265f0d8afd3ec0a41895c9741af59e749
Breno Reis <reisb6774@gmail.com>
2025-07-29 04:13:24 -0300

    feat: Introducing the main file
    This file contains the results from the AI model, separating it from the other in a way to make my code more readable and cleaner

---
+++ app.py
@@ -0,0 +1,12 @@
+# This is the main python file.
+# In a way to make my code more clean and flexible, I have separated this project in two files
+# The result shall be displayed in this script
+from data_ai_model import ai_model # Importing the script from the other file
+
+
+def main():
+    ai_model()
+
+
+
+if __name__ == '__main__': main()
